b0042886f5bcfd22dcdf6b48081ec7e4c237f7ad
Herculano <herculano.debiasi@unoesc.edu.br>
2025-10-19 14:56:51 -0400

    teste railway

--- index.php
+++ index.php
@@ -65,8 +65,8 @@ if(isset($_POST['logar'])) {
         </form>
     </div>
     
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
     
     <script>
         $(document).ready(function() {
