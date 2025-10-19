9a0941d6f61a47eb61d3bdb0ce38d72c07213263
Herculano <herculano.debiasi@unoesc.edu.br>
2025-10-19 09:47:52 -0400

    Suporte a conexão em ambiente de produção no Railway

--- dados_acesso.php
+++ dados_acesso.php
@@ -1,6 +1,7 @@
 <?php
 define('DSN', 'mysql'); // DSN: Database Source Name
-define('SERVIDOR', 'localhost');
-define('USUARIO', 'root');
-define('SENHA', 'root');
-define('BANCODEDADOS', 'unoesc');
+define('SERVIDOR', $_ENV['MYSQLHOST']);
+define('USUARIO', $_ENV['MYSQLUSER']);
+define('SENHA', $_ENV['MYSQLPASSWORD']);
+define('BANCODEDADOS', $_ENV['MYSQLDATABASE']);
+define('PORTA', $_ENV['MYSQLPORT']);
