d69bc5b254a42ba890b556a8a42ec71fe0098233
Herculano <herculano.debiasi@unoesc.edu.br>
2025-10-19 12:01:07 -0400

    teste conexao

--- dados_acesso.php
+++ dados_acesso.php
@@ -1,7 +1,7 @@
 <?php
 define('DSN', 'mysql'); // DSN: Database Source Name
-define('SERVIDOR', $_ENV['MYSQLHOST']);
+/*define('SERVIDOR', $_ENV['MYSQLHOST']);
 define('USUARIO', $_ENV['MYSQLUSER']);
 define('SENHA', $_ENV['MYSQLPASSWORD']);
 define('BANCODEDADOS', $_ENV['MYSQLDATABASE']);
-define('PORTA', $_ENV['MYSQLPORT']);
+define('PORTA', $_ENV['MYSQLPORT']);*/
